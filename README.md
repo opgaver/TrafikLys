6d71d095c66e49d972a1a638cac8f0e790d39c87
Michell Cronberg <michell@cronberg.dk>
2024-03-29 09:46:23 +0100

    Add TrafficLight class and README.md file

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # TrafikLys
+
+Dette er en simpel opgave i at skrive en klasse, der repræsenterer et trafiklys i C#. I datamatiker/datalog uddannelsen er dette første skridt i at lære om objektorienteret programmering (OOP) samt tilstandsmaskiner.
+
+## Opgave
+
+Du skal skrive en klasse `TrafficLight`, der repræsenterer et trafiklys. Trafiklyset skal have tre tilstande: `Red`, `RedYellow`, `Green` og `Yellow`. Trafiklyset skal have en metode `ChangeState()`, der skifter til næste tilstand. 
+
+Du kan læse lidt om (danske) trafiklys på [Wikipedia](https://da.wikipedia.org/wiki/Trafiksignal), men her er en kort beskrivelse af de forskellige tilstande (billedet er fra artiklen):
+
+![](https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Traffic_lights_4_states.png/220px-Traffic_lights_4_states.png)
+
+Du bestemmer selv timingen for skift mellem tilstandene - men i min løsning har jeg valgt at skifte tilstand som følger:
+
+- `Red` -> `RedYellow` (efter 5 sekunder)
+- `RedYellow` -> `Green` (efter 2 sekunder)
+- `Green` -> `Yellow` (efter 5 sekunder)
+- `Yellow` -> `Red` (efter 2 sekunder)
+
+men det kan selvfølgelig ændres til noget der simulerer virkeligheden.
+
+Du kan se en gif af min løsning her, og kan naturligvis kigge på min kode under solution-mappen - men prøv at se hvad du kan skabe uden at kigge først.
+
+<div>
+    <video src='/solution/mcronberg/TrafficLight/trafiklys.mp4' />
+</div>
+
+Du er velkommen til at skabe en PR med din løsning, hvis du vil dele den. Du skal bare sørge for, at den ligger i Solution-mappen i en folder med dit navn.
---
+++ solution/mcronberg/TrafficLight/Program.cs
@@ -0,0 +1,76 @@
+﻿TraficLight trafikLys = new TraficLight();
+Console.ReadLine();
+
+class TraficLight
+{
+
+    public TraficLightState State { get; private set; }
+
+    private System.Timers.Timer Timer = new System.Timers.Timer();
+    private int[] Timing;
+
+    public TraficLight(int[]? timing = null)
+    {
+        this.Timing = timing ?? new int[] { 5000, 2000, 5000, 2000 };
+        this.State = TraficLightState.Red;
+        this.Timer.Interval = this.Timing[0];
+        this.Timer.Elapsed += (sender, e) => ChangeState();
+        this.Timer.Start();
+        PrintState();
+    }
+
+
+    public void ChangeState()
+    {
+
+        switch (this.State)
+        {
+            case TraficLightState.Red:
+                this.State = TraficLightState.RedYellow;
+                this.Timer.Interval = this.Timing[1];
+                break;
+            case TraficLightState.RedYellow:
+                this.State = TraficLightState.Green;
+                this.Timer.Interval = this.Timing[2];
+                break;
+            case TraficLightState.Green:
+                this.State = TraficLightState.Yellow;
+                this.Timer.Interval = this.Timing[3];
+                break;
+            case TraficLightState.Yellow:
+                this.State = TraficLightState.Red;
+                this.Timer.Interval = this.Timing[0];
+                break;
+            default:
+                throw new TrafficLightException("Unknown state");
+        }
+        PrintState();
+    }
+
+    public void Reset()
+    {
+        this.State = TraficLightState.Red;
+    }
+
+    private void PrintState()
+    {
+        Console.WriteLine(this.State);
+    }
+
+}
+
+enum TraficLightState
+{
+    Red,
+    RedYellow,
+    Green,
+    Yellow
+}
+
+public class TrafficLightException : Exception
+{
+    public TrafficLightException(string message)
+        : base(message)
+    {
+    }
+}
